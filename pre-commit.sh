b8b40dd2bf2e46f0a1faa81607eef1634381e5e8
Rick Clare <rick@wildcountrytech.co.uk>
2015-02-05 12:10:57 +0000

    updating pre-commit again

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # When cloning this repository for the first time, install this commit hook:
-#   mkdir -p .git/hooks && ln -s ./pre-commit.sh .git/hooks/pre-commit && chmod +x .git/hooks/pre-commit
+#   mkdir -p .git/hooks && ln -sf ../../pre-commit.sh .git/hooks/pre-commit && chmod +x .git/hooks/pre-commit
 
 ### Are any Sass files canged and added?
 if git status -s | grep  '^M.\+\.haml$'
