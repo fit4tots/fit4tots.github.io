29d8695e450c815cfd353cee02d9ca02c53c54d6
Rick Clare <rick@wildcountrytech.co.uk>
2015-02-05 12:46:17 +0000

    test precommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,12 +3,12 @@
 # When cloning this repository for the first time, install this commit hook:
 #   mkdir -p .git/hooks && ln -sf ../../pre-commit.sh .git/hooks/pre-commit && chmod +x .git/hooks/pre-commit
 
-### Are any Sass files canged and added?
+### Are any HAML files changed or added?
 if git status -s | grep '^M.\+\.haml$'
 then
   # bundle exec rake build
   rake build
-  find . -type f -name "*.html" | grep -v "\_site" | xargs git add
+  find . -type f -name "*.html" | grep -v "_site" | xargs git add
 fi
 
 exit 0
